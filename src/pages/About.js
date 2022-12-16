0ad81c3cfb94359dc512e251413f421cbe483aaa
Saoa35 <saoamah@gmail.com>
2022-12-16 22:34:51 +0100

    created Form comp.

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+export const About = () => (
+  <div className="bg-light">
+    <div className="container">
+      <h1 className="display-4">The best React App</h1>
+      <p className="lead">
+        Version <strong>1.0.38</strong>
+      </p>
+    </div>
+  </div>
+);
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import { Fragment } from "react";
+
+export const Home = () => {
+  return (
+    <Fragment>
+      <h1>Home Page</h1>
+    </Fragment>
+  );
+};
