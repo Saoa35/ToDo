91010e52713d0e6d8a62532845155b27bc1a751e
Saoa35 <saoamah@gmail.com>
2022-12-16 20:05:21 +0100

    created Home comp.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container pt-4">
+        <h1>APP</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
