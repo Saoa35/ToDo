a33202f9242a269f0370be72e83a58090c6b8eb6
Saoa35 <saoamah@gmail.com>
2022-12-17 19:39:25 +0100

    created firebaseReducer

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AlertContext } from "../context/alert/alertContext";
 
 export const Form = () => {
   const [value, setValue] = useState("");
+  const alert = useContext(AlertContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (value.trim()) {
+      // ...
+      alert.show("Note has been created", "success");
+      setValue("");
+    } else {
+      alert.show("Enter note");
+    }
+  };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="form-group">
         <input
           className="form-control"
