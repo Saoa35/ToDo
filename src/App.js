ba34e7a9272b43a33bde72402a3077c64d6ce78f
Saoa35 <saoamah@gmail.com>
2022-12-17 16:23:05 +0100

    + AlertState to App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Alert } from "./components/Alert";
 import { Navbar } from "./components/Navbar";
+import { AlertState } from "./context/alert/AlertState";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="container pt-4 ms-4">
-        <Alert />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container pt-4 ms-4">
+          <Alert />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
