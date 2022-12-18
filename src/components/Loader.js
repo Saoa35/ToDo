ec70a5c6876589b34c0831d8e5bd6db848824c9d
Saoa35 <saoamah@gmail.com>
2022-12-18 15:30:44 +0100

    created Loader comp.

---
+++ src/components/Loader.js
@@ -0,0 +1,7 @@
+export const Loader = () => (
+  <div className="text-center">
+    <div class="spinner-border text-primary" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
-import { Fragment } from "react";
+import { Fragment, useContext, useEffect } from "react";
 import { Form } from "../components/Form";
 import { Notes } from "../components/Notes";
+import { FirebaseContext } from "../context/firebase/firebaseContext";
 
 export const Home = () => {
-  const notes = new Array(3)
-    .fill("")
-    .map((_, i) => ({ id: i, title: `Note ${i + 1}` }));
+  const { loading, notes, fetchNotes } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    fetchNotes();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <Fragment>
       <Form />
